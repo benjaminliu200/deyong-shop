a17df78b835d1f609521d33dadaefa89f96f2316
Benjamin <1092774861@qq.com>
2017-01-04 23:13:20 +0800

    单元测试中mybatis 的 分页插件使用

--- commfile/GeneratorSqlMap.java
+++ commfile/GeneratorSqlMap.java
@@ -6,17 +6,17 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * Created by benjamin on 2017/1/4.
  */
-public class GeneratorSqlMapCustom {
+public class GeneratorSqlMap {
+
     public void generator() throws Exception {
         List<String> warnings = new ArrayList<>();
         boolean overwrite = true;
-        File configFile = new File(String.valueOf(getClass().getClassLoader().getResourceAsStream("sqlmap.xml")));
-
+//        File configFile = new File(String.valueOf(getClass().getClassLoader().getResourceAsStream("sqlmap.xml")));
+        File configFile = new File("E:\\javaIDE\\ideawork\\xbb\\deyong-manager-pojo\\src\\main\\generatorConfig.xml");
 
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
@@ -24,8 +24,9 @@ public class GeneratorSqlMapCustom {
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
     }
-
     public static void main(String[] args) throws Exception {
-        new GeneratorSqlMapCustom().generator();
+
+        GeneratorSqlMap sqlMap = new GeneratorSqlMap();
+        sqlMap.generator();
     }
 }
---
+++ deyong-manager-service/src/main/java/com/deyong/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.deyong.service;
+
+import com.deyong.pojo.TbItem;
+
+/**
+ * Created by Benjamin on 2017/1/4.
+ */
+public interface ItemService {
+    TbItem getItemById(Long itemId);
+}
