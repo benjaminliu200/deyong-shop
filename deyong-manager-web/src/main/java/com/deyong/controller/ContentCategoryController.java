def50668c1c3de46f110d5e983a3526603ac8be7
benjamin <1092774861@qq.com>
2017-01-16 15:01:33 +0800

    下班提交

---
+++ deyong-manager-web/src/main/java/com/deyong/controller/ContentCategoryController.java
@@ -0,0 +1,35 @@
+package com.deyong.controller;
+
+import com.deyong.service.ContentCategoryService;
+import com.ldy.common.pojo.EUTreeNode;
+import com.ldy.common.util.DeyongResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by benjamin on 2017/1/16.
+ */
+@Controller
+@RequestMapping("/content/category")
+public class ContentCategoryController {
+
+    @Autowired
+    private ContentCategoryService contentCategoryService;
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<EUTreeNode> getContentCatList(@RequestParam(value = "id", defaultValue = "0") long parentId) {
+        return contentCategoryService.getContentCategory(parentId);
+    }
+
+    @RequestMapping("/create")
+    @ResponseBody
+    public DeyongResult insertContentCat(long parentId, String name) {
+        return contentCategoryService.insertContentCat(parentId, name);
+    }
+}
