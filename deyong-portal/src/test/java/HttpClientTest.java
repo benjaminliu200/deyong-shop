d8d6b2e7070bd646bee2ffdd18b9d858705b9b0a
Benjamin <1092774861@qq.com>
2017-01-16 22:43:49 +0800

    提交楼下

--- deyong-portal/src/test/java/HttpClientTest.java
+++ deyong-portal/src/test/java/HttpClientTest.java
@@ -3,11 +3,13 @@ import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
+import java.net.URI;
 import java.util.Arrays;
 
 /**
@@ -31,5 +33,35 @@ public class HttpClientTest {
         System.out.println(s);
         // 关闭httpClient
         client.close();
+    }
+
+    @Test
+    public void testGetParam() throws Exception {
+        // 创建Httpclient对象
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        // 定义请求的参数
+        URI uri = new URIBuilder("http://www.baidu.com/s").setParameter("wd", "java").build();
+
+        System.out.println(uri);
+
+        // 创建http GET请求
+        HttpGet httpGet = new HttpGet(uri);
+
+        CloseableHttpResponse response = null;
+        try {
+            // 执行请求
+            response = httpclient.execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
+                System.out.println(content);
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+            httpclient.close();
+        }
     }
 }
